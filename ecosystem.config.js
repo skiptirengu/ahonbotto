9dfe4b648c2765d3e985f33cc979683de8c3b174
Thiago Oliveira <thiago_ogt@outlook.com>
2019-09-19 02:37:33 -0300

    Add pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Ahonbotto',
+      script: 'yarn',
+      args: 'start',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      ignore_watch: ['node_modules']
+    }
+  ]
+}
