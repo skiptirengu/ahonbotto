69720bedcf2e8ed0df9d0f23cf180dfe721450c6
Thiago <skiptirengu@users.noreply.github.com>
2019-09-20 16:46:59 -0300

    watch is doing weird stuff

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,12 @@ module.exports = {
       script: 'yarn',
       args: 'start',
       instances: 1,
-      autorestart: true,
-      watch: true,
+      autorestart: true
+      // watch: true,
       // eslint-disable-next-line @typescript-eslint/camelcase
-      ignore_watch: ['node_modules', 'dist', 'runtime'],
+      // ignore_watch: ['node_modules', 'dist', 'runtime'],
       // eslint-disable-next-line @typescript-eslint/camelcase
-      restart_delay: 3000
+      // restart_delay: 3000
     }
   ]
 }
