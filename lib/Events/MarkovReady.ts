a8623f45b3ada2739e271e10c7e912b2e145c015
Thiago Teixeira <thiago_ogt@outlook.com>
2020-08-15 18:40:44 -0300

    more logs

--- lib/Events/MarkovReady.ts
+++ lib/Events/MarkovReady.ts
@@ -11,24 +11,33 @@ export const markovReady = (client: Client): void => {
   const markovRepository = container.resolve(MarkovChainRepository);
   const config = container.resolve<Config>('Config');
   const pending = markovRepository.getGuildPending(config.markovSentenceCacheSize);
+
   const promises = pending.map(async (markov) => {
     const guild = client.guilds.resolve(markov.guild);
     if (!guild) throw new Error(`Guild ${markov.guild} does not exist`);
 
     const scope = scopeFactory(guild);
     const logger = scope.resolve<Logger>('Logger');
+
     const messageSource = scope.resolve(SynchronizedSentenceSource);
     logger.info('warming up cache for guild', { id: guild.id, name: guild.name });
     messageSource.warmUpCache(markov.id);
 
+    const limit = messageSource.getRamainingCount();
+    if (!limit) return;
+
     const resolver = scope.resolve(MarkovMessageResolver);
     const firstMessage = markovRepository.getFirstMessage(markov.id);
-    const limit = messageSource.getRamainingCount();
     logger.info('not enough cached messages: importing from discord', { count: limit });
     await resolver.resolveMessages(markov, firstMessage, limit);
   });
 
   const logger = container.resolve<Logger>('Logger');
+
+  if (promises.length) {
+    logger.info('message import not required for any guild');
+    return;
+  }
 
   Promise.all(promises)
     .then(() =>
